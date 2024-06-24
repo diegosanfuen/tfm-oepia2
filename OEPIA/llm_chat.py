e3df97439679ccd9d7d6e5466611bfa020505475
Diego Sánchez <63495248+diegosanfuen@users.noreply.github.com>
2024-06-24 17:44:18 +0200

    Segunda Inclusion lanzamiento de datos.py

--- OEPIA/llm_chat.py
+++ OEPIA/llm_chat.py
@@ -341,9 +341,11 @@ def chat(pregunta):
                 logger.debug(str("\n".join(sesiones.obtener_mensajes_por_sesion(token))))
                 # response = llmApp.invoke({"input": pregunta,
                 #                          "context": str("\n".join(sesiones.obtener_mensajes_por_sesion(token)))})
-                response = llmAppAgent.invoke({"input": pregunta,
-                                          "context": str("\n".join(sesiones.obtener_mensajes_por_sesion(token)))},
-                                         {'configurable': {'session_id': f'{token}'}})
+                response = llmAppAgent.invoke({{"input": "Ten en cuenta la siguiente información como contexto, pero no la incluyas en tus respuestas, si se te solicita una operación concreta omite el conexto: " +
+                                               "<context>" + str('\n'.join(sesiones.obtener_mensajes_por_sesion(token, k=10))) +
+                                               "</context>\n" + pregunta,
+                                      "context": str("\n".join(sesiones.obtener_mensajes_por_sesion(token)))},
+                                     {'configurable': {'session_id': f'{token}'}})
                 answer = str(response['answer'])
                 sesiones.add_mensajes_por_sesion(token, str(f"HumanMessage: {pregunta}"))
                 sesiones.add_mensajes_por_sesion(token, str(f"AIMessage: {answer}"))
