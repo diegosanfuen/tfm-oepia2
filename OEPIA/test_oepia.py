e87810dc5afa432806819e8fbc7cd6508ab2fb1c
Diego Sánchez <63495248+diegosanfuen@users.noreply.github.com>
2024-06-26 22:00:55 +0200

    Segunda Inclusion lanzamiento de datos.py

--- OEPIA/test_oepia.py
+++ OEPIA/test_oepia.py
@@ -17,11 +17,11 @@ class TestOepia(unittest.TestCase):
     def test_obtener_boe_texto(self):
         self.assertEqual(len(utls.obtener_boe_texto("https://www.boe.es//boe/dias/2024/05/23/pdfs/BOE-A-2024-10363.pdf") > 1), True)
 
-     def test_obtener_ayuda_oepia(self):
-         self.assertEqual(len(utls.obtener_ayuda_oepia() > 1), True)
+    def test_obtener_ayuda_oepia(self):
+        self.assertEqual(len(utls.obtener_ayuda_oepia() > 1), True)
 
-     def test_obtenerPROMPTMemoriaContextoOEPIA(self):
-         self.assertEqual(len(utls.obtenerPROMPTMemoriaContextoOEPIA('1234567890acbd') > 1), True)
+    def test_obtenerPROMPTMemoriaContextoOEPIA(self):
+        self.assertEqual(len(utls.obtenerPROMPTMemoriaContextoOEPIA('1234567890acbd') > 1), True)
 
 # Ejecutar los tests
 if __name__ == '__main__':
