bc38c1115abc344e6e9ac6323846181d2aed6124
Diego Sánchez <63495248+diegosanfuen@users.noreply.github.com>
2024-06-26 22:26:46 +0200

    Segunda Inclusion lanzamiento de datos.py

---
+++ FaissOPEIA/test_FaissOEPIA.py
@@ -0,0 +1,23 @@
+import sys, os
+sys.path.insert(0, os.environ['PROJECT_ROOT'])
+
+import unittest
+import shutil
+from FaissOepia.carga import carga as carga
+# from FaissOepia.ingesta import ingesta as ingesta
+
+class TestFaiss(unittest.TestCase):
+
+    def test_carga(self):
+        try:
+            origen = os.environ['PROJECT_ROOT'] + "/FaissOEPIA/test/*"
+            destino = os.environ['PROJECT_ROOT'] + "/FaissOEPIA/db/"
+            shutil.copy(origen, destino)
+            BDVect = carga()
+            retriever = BDVect.getRetriver()
+            assert 'retriever' in globals() or 'retriever' in locals()
+        except:
+            raise AssertionError
+
+
+
